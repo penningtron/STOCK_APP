1b9b014e4b6deb171b86be7ccad2e7be5f451a10
nathan pennington <nathantp@gmail.com>
2024-04-17 16:51:59 -0500

    working

--- front_end/src/pages/SignUpPage.jsx
+++ front_end/src/pages/SignUpPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { api } from '../utilities';
 import { useOutletContext } from 'react-router-dom';
+import { userLogin } from '../utilities'; // Import the userLogin function
+import { useNavigate } from 'react-router-dom'; // Import the navigate function
 
 function SignUpForm() {
   const [first_name, setFirstName] = useState('');
@@ -10,19 +12,61 @@ function SignUpForm() {
   const [password, setPassword] = useState('');
   const [phone_number, setPhoneNumber] = useState('');
   const { setUser } = useOutletContext();
+  const navigate = useNavigate();
 
   const signupUser = async(e) => {
     e.preventDefault();
     const response = await api.post("users/signup/", { first_name, last_name, email, password, phone_number });
     if (response.status === 201) {
       alert("User created successfully");
-      const { token, user } = response.data;
-      localStorage.setItem("token", token);
-      api.defaults.headers.common['Authorization'] = `Token ${token}`;
-      // set user info for the app
-      setUser({ email, user })
+      const { user } = await userLogin(email, password);
+        
+        
+      if (user) {
+        console.log(user);
+        setUser(user);
+        localStorage.setItem("user", user);
+        //comment the line below to get it kind of working
+          // localStorage.setItem("user", JSON.stringify(user));
+          alert("Login successful");
+          navigate('/homepage');
+      }
+      
+      
+      // const { token, email } = response.data;
+      // localStorage.setItem("token", token);
+      // api.defaults.headers.common['Authorization'] = `Token ${token}`;
+      // console.log(email);
+      // setUser({ email })
     }
   };
+
+  // const signupUser = async (email, password, phone_number, first_name, last_name) => {
+  //   try {
+  //     const signupResponse = await axios.post('http://127.0.0.1:8000/api/v1/users/signup/', {
+  //       email: email,
+  //       password: password,
+  //       phone_number: phone_number,
+  //       first_name: first_name,
+  //       last_name: last_name,
+  //     });
+  
+     
+
+  //       if (signupResponse.status === 201) {
+  //       const { user } = await userLogin(email, password);
+  //       if (user) {
+  //         console.log(user);
+  //         setUser(user);
+  //         localStorage.setItem("user", JSON.stringify(user));
+  //         alert("Signup and login successful");
+  //         navigate('/homepage'); // Use the navigate function to navigate to '/homepage'
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error('Error signing up user:', error);
+  //   }
+  // };
   
 
 
